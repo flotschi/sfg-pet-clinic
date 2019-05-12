a73aa5cc89e705d475b56e99470ed2d2209d72a4
NEO <neo.example.com>
2019-05-12 10:44:42 +0200

    updated profiles for JPA service, tested active profiles closes#48

--- pet-clinic-data/src/main/java/com/flo/good/sfgpetclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/com/flo/good/sfgpetclinic/services/map/PetTypeMapService.java
@@ -2,11 +2,13 @@ package com.flo.good.sfgpetclinic.services.map;
 
 import com.flo.good.sfgpetclinic.model.PetType;
 import com.flo.good.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
 
     @Override
